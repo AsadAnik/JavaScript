240788a4b05cbe3b7945752dabb055efd6957b50
asadanik <s.engineer63@gmail.com>
2019-12-31 15:21:24 +0600

    javascriptcode

--- Functional JavaScript/ArraySorting.js
+++ Functional JavaScript/ArraySorting.js
@@ -1,6 +1,11 @@
 ///The Array list here...
 var arr = [234, 120, 34, 54, 9, 10, 01, 23, 7,  12, 45, 90, 100, 345, 122]
-
+///Default sorting....
 arr.sort()
-
 console.log(arr);
+
+///Custom Sorting Ways....
+arr.sort(function(a, b){
+    return b-a;
+})
+console.log(arr)
